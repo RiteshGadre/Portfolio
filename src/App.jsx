9b14e876cb83dec89bb6ee4cc01f74ffaa278e60
RiteshGadre <riteshgadre9@gmail.com>
2024-09-06 18:00:57 +0530

    Navbar is completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import Header from "./components/Header"
 
 
 function App() {
   
 
   return (
-    <div>
-      <h1 className="bg-red-500">Hello World</h1>
+    <div className="min-h-screen">
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import Logo from "../assets/Logo.png"
+
+const Header = () => {
+  return (
+    <div className="flex justify-between items-center w-full bg-[#505f6e] text-white h-16">
+      <nav className="font-mono px-4 flex justify-between items-center w-full flex-wrap">
+        <div className="logo max-w-9">
+            {/* <img src={Logo} alt="" /> */}
+            <h1 className="font-serif font-bold text-2xl">RITESH</h1>
+        </div>
+        <ul className="flex gap-6 font-semibold">
+            <li className="cursor-pointer">Home</li>
+            <li className="cursor-pointer">Skills</li>
+            <li className="cursor-pointer">Projects</li>
+            <li className="cursor-pointer">DSA</li>
+            <li className="cursor-pointer">Blogs</li>
+        </ul>
+        <button className="rounded-full px-4 py-1 font-bold bg-white text-[#505f6e]">
+            Resume
+        </button>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
